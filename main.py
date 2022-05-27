925eb9cc1bffc662b733663cc9c961ea039262fd
Arnav Gupta <Arnav000777gupta@gmail.com>
2022-05-28 00:03:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/c/<class>')
-def c(c):
-    return render_template('index.html')
 
 app.run(host='0.0.0.0', port=81)
